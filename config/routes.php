b19d17ca79d126d4b7ea7bbcbc6582b4456bd274
Ilon Maks <macsimgolubckov@gmail.com>
2025-06-11 01:30:04 +0300

    add router v1

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Controllers\homeController;
+use App\Controllers\movieController;
+use App\Kernel\Router\Route;
+return [
+    Route::get('/user', [homeController::class, 'user']),
+    Route::get('/user/{id}', [movieController::class, 'user']),
+    Route::post('/user/{id}', [movieController::class, 'add']),
+    Route::put('/user/{id}', [movieController::class, 'reWrite'])
+    Route::delete('/user/{id}', [movieController::class, 'delete']),
+
+];
